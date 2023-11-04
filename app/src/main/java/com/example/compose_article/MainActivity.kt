f1ef36cd90701558e870c00e4273288df7b9c467
mochizuki <c0a21124f8@edu.teu.ac.jp>
2023-11-04 22:44:36 +0900

    演習その１完了

--- app/src/main/java/com/example/compose_article/MainActivity.kt
+++ app/src/main/java/com/example/compose_article/MainActivity.kt
@@ -3,14 +3,26 @@ package com.example.compose_article
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.compose_article.ui.theme.Compose_articleTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +34,65 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ArticleImage(
+                        title = stringResource(R.string.article_title),
+                        text_1 = stringResource(R.string.article_text_1),
+                        text_2 = stringResource(R.string.article_text_2))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Compose_articleTheme {
-        Greeting("Android")
+        ArticleImage(
+            title = "Jetpack Compose tutorial",
+            text_1 = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+            text_2 = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name."
+        )
     }
 }
+
+@Composable
+fun ArticleText(title: String,text_1: String,text_2: String, modifier: Modifier = Modifier) {
+    Column() {
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+        Text(
+            text = text_1,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+        Text(
+            text = text_2,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+    }
+}
+
+@Composable
+fun ArticleImage(title: String,text_1: String,text_2: String, modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.bg_compose_background)
+    Column{
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        ArticleText(
+            title = title,
+            text_1 = text_1,
+            text_2 = text_2,
+        )
+    }
+}
+
